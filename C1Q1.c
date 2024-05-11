4f7a1da36acbf720522d293f449d5d3521119540
Surya Pratap Singh <72143906+surya123-ctrl@users.noreply.github.com>
2024-05-11 09:29:47 +0530

    Completed C1Q1

--- C1Q1.c
+++ C1Q1.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 int main()
 {
-    printf("Hello World");
-    int n;
-    scanf("%d", &n);
-    printf("%d", n);
+    int basic_salary = 0;
+    printf("Enter Basic Salary : ");
+    scanf("%d", &basic_salary);
+    float dearness_allowance = 0.4 * basic_salary;
+    float house_rent_allowance = 0.2 * basic_salary;
+    float gross_salary = basic_salary + dearness_allowance + house_rent_allowance;
+    printf("Gross Salary : %f", gross_salary);
+    return 0;
 }
